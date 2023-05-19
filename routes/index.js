a2e1858122fd10975eb1f2c0766512411423de31
AndrewRoberts1 <92851210+AndrewRoberts1@users.noreply.github.com>
2023-05-19 16:05:27 +0000

    added session to index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const mysql = require('mysql');
 const dbclient = require('./database');
 const bcrypt = require('bcryptjs');
 var appJS = require('../app');
