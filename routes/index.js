02fed91efd193407ad7f2d621341ee923663c399
AndrewRoberts1 <92851210+AndrewRoberts1@users.noreply.github.com>
2023-05-20 13:56:12 +0000

    more redirects to login added

--- routes/index.js
+++ routes/index.js
@@ -132,7 +132,7 @@ router.get('/customer_account', function(req, res, next) {
       } else {
         var user = result.rows[0]
         // Render the pug template file with the database results
-        res.render('customer_create', { 
+        res.render('customer_account', { 
           first_name: user.first_name,
           surname: user.second_name,
           email: user.email,
@@ -142,6 +142,7 @@ router.get('/customer_account', function(req, res, next) {
       }
     });
   } else {
+    // Redirect user to login if not already
     res.redirect('/customer_login');
   }
   
@@ -176,7 +177,7 @@ router.post('/add_user', function(req, res, next) {
           console.log('Error querying the database:', err);
           res.send(500);
         } else {
-          // Render the pug template file with the database results
+          // Redirect user to login if not already
           res.redirect('/customer_login');
         }
       });
@@ -188,63 +189,73 @@ router.post('/add_user', function(req, res, next) {
 
 // GET basket page
 router.get('/basket', function(req, res, next) {
+  if (req.session.customer_id) {
 
-  // Make a database query
-  var sql = `SELECT * FROM basket
-  LEFT JOIN size_options as size ON size.size_id = basket.size_id
-  LEFT JOIN product ON product.product_id = size.product_id
-  WHERE basket.customer_id = 17`;
-  //Execute db query
-  dbclient.query(sql, (err, result) => {
-    //Check for error in db query
-    if (err) {
-      //display the error
-      console.log('Error querying the database:', err);
-      res.send(500);
-    } else {
-      //get total of basket
-      let sum = 0;
-      for (item in result.rows) {
-        sum += result.rows[item].price * result.rows[item].quantity;
+    // Make a database query
+    var sql = `SELECT * FROM basket
+    LEFT JOIN size_options as size ON size.size_id = basket.size_id
+    LEFT JOIN product ON product.product_id = size.product_id
+    WHERE basket.customer_id = 17`;
+    //Execute db query
+    dbclient.query(sql, (err, result) => {
+      //Check for error in db query
+      if (err) {
+        //display the error
+        console.log('Error querying the database:', err);
+        res.send(500);
+      } else {
+        //get total of basket
+        let sum = 0;
+        for (item in result.rows) {
+          sum += result.rows[item].price * result.rows[item].quantity;
+        }
+        // Render the pug template file with the database results
+        res.render('basket', { 
+          basket_list: result.rows,
+          subTotal: sum
+        });
       }
-      // Render the pug template file with the database results
-      res.render('basket', { 
-        basket_list: result.rows,
-        subTotal: sum
-      });
-    }
-  });
+    });
+  } else {
+    // Redirect user to login if not already
+    res.redirect('/customer_login');
+  }
 });
 
 router.get('/checkout', async function(req, res, next) {
-  var customer_query = await resultQuery("SELECT * FROM customers WHERE customer_id = 17");
-  // Make a database query
-  var sql = `SELECT * FROM basket
-  LEFT JOIN size_options as size ON size.size_id = basket.size_id
-  LEFT JOIN product ON product.product_id = size.product_id
-  WHERE basket.customer_id = 17`;
-  //Execute db query
-  dbclient.query(sql, (err, result) => {
-    //Check for error in db query
-    if (err) {
-      //display the error
-      console.log('Error querying the database:', err);
-      res.send(500);
-    } else {
-      //get total of basket
-      let sum = 0;
-      for (item in result.rows) {
-        sum += result.rows[item].price * result.rows[item].quantity;
+  if (req.session.customer_id) {
+    var customer_query = await resultQuery("SELECT * FROM customers WHERE customer_id = 17");
+    // Make a database query
+    var sql = `SELECT * FROM basket
+    LEFT JOIN size_options as size ON size.size_id = basket.size_id
+    LEFT JOIN product ON product.product_id = size.product_id
+    WHERE basket.customer_id = 17`;
+    //Execute db query
+    dbclient.query(sql, (err, result) => {
+      //Check for error in db query
+      if (err) {
+        //display the error
+        console.log('Error querying the database:', err);
+        res.send(500);
+      } else {
+        //get total of basket
+        let sum = 0;
+        for (item in result.rows) {
+          sum += result.rows[item].price * result.rows[item].quantity;
+        }
+        
+        // Render the pug template file with the database results
+        res.render('basket', {
+          customer_details: customer_query.rows[0],
+          basket_list: result.rows,
+          subTotal: sum
+        });
       }
-      
-      // Render the pug template file with the database results
-      res.render('basket', {
-        customer_details: customer_query.rows[0],
-        basket_list: result.rows,
-        subTotal: sum
-      });
-    }
-  });
+    });
+  } else {
+    // Redirect user to login if not already
+    res.redirect('/customer_login');
+  }
 });
 
  
